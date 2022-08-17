78c543af23ec2f3f97c1836f9d15fcf777435127
tamaki05 <kx0c0xk@gmail.com>
2022-08-17 09:33:04 +0000

    topの画像拡大表示機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  # # 検索用
-  # before_action :set_search
+  # 検索用
+  before_action :set_search
 
-  # def set_search
-  #   @search = Exhibition.ransack(params[:q])
-  #   @search_exhibitions = @search.result.page(params[:page])
-  # end
+  def set_search
+    @search = Exhibition.ransack(params[:q])
+    # @search_exhibitions = @search.result.page(params[:page])
+  end
 
 end
