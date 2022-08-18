0d7854916b22cb03db3fafa55da3254ed6cb9811
tamaki05 <kx0c0xk@gmail.com>
2022-08-18 08:20:07 +0000

    カテゴリー一覧画像リンク実装

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
   has_many :exhibitions
   validates :name, presence: true
+
+  def get_image( width, height)
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
