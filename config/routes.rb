7c280566862fda1c2b5c2799e326eba2c58a2c26
tamaki05 <kx0c0xk@gmail.com>
2022-08-20 09:50:08 +0000

    フォロー機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,9 @@ Rails.application.routes.draw do
     get 'users/unsubscribe' => 'users#unsubscribe'
     patch 'users/withdraw' => 'users#withdraw'
     resources :users, only:[:index, :show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+        get 'followings' => 'relationships#followings', as: 'followings'
+        get 'followers' => 'relationships#followers', as: 'followers'
       member do
         get :favorites
         get :exhibitions
