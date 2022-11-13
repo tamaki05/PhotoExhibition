43f1849fb004e75f9b504940ffd104e2e1790516
tamaki05 <kx0c0xk@gmail.com>
2022-11-13 05:00:15 +0000

    userのshowにメッセージ送るリンク追加

--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -1,5 +1,10 @@
 class Public::RoomsController < ApplicationController
+  before_action :authenticate_user!
   def create
+    # room = Room.create
+    # current_entry = Entry.create(user_id: current_user.id, room_id: room.id)
+    # another_entry = Entry.create(user_id: params[:entry][:user_id], room_id: room.id)
+    # redirect_to room_path(room)
   end
 
   def index
